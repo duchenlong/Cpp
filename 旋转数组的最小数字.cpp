41bce6b0bc2702fcc280bdd5caa768e858968e65
duchenlong <2664484273@qq.com>
2020-07-22 12:16:31 +0800

    旋转数组的最小数字

---
+++ 旋转数组的最小数字.cpp
@@ -0,0 +1,32 @@
+把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组?[3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。??
+
+示例 1：
+
+输入：[3,4,5,1,2]
+输出：1
+示例 2：
+
+输入：[2,2,2,0,1]
+输出：0
+
+class Solution {
+public:
+    int minArray(vector<int>& numbers) {
+        int le = 0;
+        int ri = numbers.size() - 1;
+         
+        while(le < ri)
+        {
+            int mid = (le + ri) / 2;
+
+            if(numbers[ri] < numbers[mid])
+                le = mid + 1;
+            else if(numbers[ri] > numbers[mid])
+                ri = mid;
+            else
+                ri--;
+        }
+
+        return numbers[le];
+    }
+};
