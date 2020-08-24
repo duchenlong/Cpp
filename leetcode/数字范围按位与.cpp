593a4a7c0d61249df8fbb4fafd29c676be021399
duchenlong <2664484273@qq.com>
2020-08-24 12:19:49 +0800

    数字范围按位与

---
+++ leetcode/数字范围按位与.cpp
@@ -0,0 +1,23 @@
+给定范围 [m, n]，其中 0 <= m <= n <= 2147483647，返回此范围内所有数字的按位与（包含 m, n 两端点）。
+
+示例 1:?
+
+输入: [5,7]
+输出: 4
+示例 2:
+
+输入: [0,1]
+输出: 0
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        int offset = 0;
+        while(m != n){
+            m >>= 1;
+            n >>= 1;
+            offset++;
+        }
+        return m << offset;
+    }
+};
