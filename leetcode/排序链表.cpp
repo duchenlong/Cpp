cb0d592e02f6245ffa7b82c13d370193aba30faa
duchenlong <2664484273@qq.com>
2020-11-22 17:02:59 +0800

    排序链表

---
+++ leetcode/排序链表.cpp
@@ -0,0 +1,89 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if(!head || !head->next) return head;
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* mid = slow->next;
+        slow->next = nullptr;
+        ListNode* ri = sortList(mid);
+        ListNode* le = sortList(head);
+        
+        // merge
+        ListNode* newhead = new ListNode(-1);
+        ListNode* cur = newhead;
+        
+        while(le || ri) {
+            
+            if(!le) {
+                cur->next = ri;
+                ri = nullptr;
+            } else if(!ri) {
+                cur->next = le;
+                le = nullptr;
+            } else {
+                if(le->val <= ri->val) {
+                    cur->next = le;
+                    le = le->next;
+                } else {
+                    cur->next = ri;
+                    ri = ri->next;
+                }
+
+                cur = cur->next;
+                cur->next = nullptr;
+            }
+        }
+
+        return newhead->next;
+    }
+};
+
+
+给你链表的头结点?head?，请将其按 升序 排列并返回 排序后的链表 。
+
+进阶：
+
+你可以在?O(n?log?n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
+?
+
+示例 1：
+
+
+输入：head = [4,2,1,3]
+输出：[1,2,3,4]
+示例 2：
+
+
+输入：head = [-1,5,3,4,0]
+输出：[-1,0,3,4,5]
+示例 3：
+
+输入：head = []
+输出：[]
+?
+
+提示：
+
+链表中节点的数目在范围?[0, 5 * 104]?内
+-105?<= Node.val <= 105
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sort-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
