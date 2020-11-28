378d1e18f66fdc374c01292e7c59e4e8b0f9d169
duchenlong <2664484273@qq.com>
2020-11-28 22:49:34 +0800

    数组中数字出现的次数 II

---
+++ leetcode/数组中数字出现的次数 II.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0; i < 32; i++) {
+            int cnt = 0;
+            int p = 1 << i;
+            for(auto& e : nums) if(e & p) cnt++;
+            if(cnt % 3 != 0) ans |= p;
+        }
+        return ans;
+    }
+};
+
+在一个数组 nums 中除一个数字只出现一次之外，其他数字都出现了三次。请找出那个只出现一次的数字。
+
+?
+
+示例 1：
+
+输入：nums = [3,4,3,3]
+输出：4
+示例 2：
+
+输入：nums = [9,1,7,9,7,9,7]
+输出：1
+?
+
+限制：
+
+1 <= nums.length <= 10000
+1 <= nums[i] < 2^31
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
