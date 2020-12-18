76e64eefb4e57fef9a2eda47ee0547a40847f2bc
duchenlong <2664484273@qq.com>
2020-12-18 20:09:35 +0800

    最长和谐子序列

---
+++ leetcode/最长和谐子序列.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int le = 0;
+        int n = nums.size();
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            while(le < i && nums[i] - nums[le] > 1)
+                le++;
+            if(nums[i] - nums[le] == 1)
+                ans = max(ans,i - le + 1);
+        }
+        return ans;
+    }
+};
+
+和谐数组是指一个数组里元素的最大值和最小值之间的差别正好是1。
+
+现在，给定一个整数数组，你需要在所有可能的子序列中找到最长的和谐子序列的长度。
+
+示例 1:
+
+输入: [1,3,2,2,5,2,3,7]
+输出: 5
+原因: 最长的和谐数组是：[3,2,2,2,3].
+说明: 输入的数组长度最大不超过20,000.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-harmonious-subsequence
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
